7217fc53ab95b191ad2fc7edd49766ab9e63bbf4
Jamie Pittak and Panatda Inthala <panatda@>
2018-01-08 11:07:59 -0800

    create formula converting gallons to liters

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+var gallonsToLiters= function (gallons){alert(gallons);};
+
+var gallons = prompt("How many gallons does the recipe call for?");
+var liters = gallons / 0.26417;
+var output = gallons + " gallons is equal to " + liters + " liters.";
+
+gallonsToLiters(output);
